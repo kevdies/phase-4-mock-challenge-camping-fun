51b837199a3d41bba106414c6028be51df10effe
Kevin Diesenberg <kdiesenb@gmail.com>
2023-01-18 21:50:23 -0500

    commit tree d551c7290e2800f2f7af4b7e3a7e5fdbfecb9411

--- app/controllers/signups_controller.rb
+++ app/controllers/signups_controller.rb
@@ -1,2 +1,17 @@
 class SignupsController < ApplicationController
+rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessabe_entity
+    def create
+        signup = Signup.create!(signup_params)
+        render json: signup.activity, status: :created
+    end
+
+    private
+
+    def signup_params
+        params.permit(:camper_id, :activity_id, :time)
+    end
+
+    def render_unprocessabe_entity(exception)
+        render json: { errors: exception.record.errors.full_messages}, status: :unprocessable_entity
+    end
 end
