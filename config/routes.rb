5b411998464a37cbbcfcc4a90091bddf6fe29f6c
Kevin Diesenberg <kdiesenb@gmail.com>
2023-01-18 21:50:39 -0500

    finished

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :signups
-  resources :activities
-  resources :campers
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :signups, only: :create
+  resources :activities, only: [:index, :destroy]
+  resources :campers, only: [:index, :show, :create]
+
 end
